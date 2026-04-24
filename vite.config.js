05cd4e2cc3f2b37557e77d5827c0d8aefbd5938c
hyewonlee-spec <hyewon.lee@freshfacepeople.com>
2026-04-24 14:30:48 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
 const base = process.env.GITHUB_ACTIONS === 'true' && repoName ? `/${repoName}/` : '/'
 
 export default defineConfig({
+  base: './',
   base,
   plugins: [react()],
 })
